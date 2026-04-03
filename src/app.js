ccd2b49feb868933bab62aab118f8de6ca08436c
Bhautik Vekariya <vbhautik41@gmail.com>
2026-04-03 16:11:45 +0530

    exploring get,post,patch,put,delete

--- src/app.js
+++ src/app.js
@@ -2,14 +2,41 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello this one dashboard");
+// This will only handle GET call to /user
+app.get("/user", (req,res)=>{
+    res.send("This is the get request successfully")
+});
+
+// This will only handle POST call to /user
+app.post("/user", (req,res)=>{
+    res.send({firstname:"Bhautik", lastnaem:"vekariya"})
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello hi how have you been")
+// This will only handle Delete call to /user
+app.delete("/user",(req,res)=>{
+    res.send("this is the delete request")
 })
 
-app.listen(7777,()=>{
-    console.log("server is successfully listening on port 7777....")
+// This will only handle put call to /user
+app.put("/user",(req,res)=>{
+    res.send("this is the put request")
+})
+
+// This will only handle patch call to /user
+app.patch("/user",(req,res)=>{
+    res.send("this is the patch request")
+})
+
+// this will match all the HTTP method API calls to /test
+app.use("/hello/2", (req, res) => {
+    res.send("Abrakadabara");
+});
+
+app.use("/hello", (req, res) => {
+    res.send("Hello hello hi how have you been");
+});
+
+
+app.listen(3000, () => {
+    console.log("server is successfully listening on port 3000....");
 });
