3af7010f3a1353da7b38a155ba8934559d468167
Bhautik Vekariya <vbhautik41@gmail.com>
2026-04-04 15:26:42 +0530

    palying with routes

--- src/app.js
+++ src/app.js
@@ -2,38 +2,9 @@ const express = require("express");
 
 const app = express();
 
-// This will only handle GET call to /user
-app.get("/user", (req,res)=>{
-    res.send("This is the get request successfully")
-});
-
-// This will only handle POST call to /user
-app.post("/user", (req,res)=>{
-    res.send({firstname:"Bhautik", lastnaem:"vekariya"})
-})
-
-// This will only handle Delete call to /user
-app.delete("/user",(req,res)=>{
-    res.send("this is the delete request")
-})
-
-// This will only handle put call to /user
-app.put("/user",(req,res)=>{
-    res.send("this is the put request")
-})
-
-// This will only handle patch call to /user
-app.patch("/user",(req,res)=>{
-    res.send("this is the patch request")
-})
-
-// this will match all the HTTP method API calls to /test
-app.use("/hello/2", (req, res) => {
-    res.send("Abrakadabara");
-});
-
-app.use("/hello", (req, res) => {
-    res.send("Hello hello hi how have you been");
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params)
+    res.send({firstname:"tushar",lastname:"makvana"})
 });
 
 
