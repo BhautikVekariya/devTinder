93d9ddc2edc79273404c90cf372d4547ef4afbcb
Bhautik Vekariya <vbhautik41@gmail.com>
2026-04-06 18:34:51 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const app = express();
 
-// Handle Auth Middleware for all GET, POST, ... request
-const {adminAuth,userAuth} = require("./middlewares/auth")
-app.use("/admin", adminAuth);
 
-app.get("/user/login",(req,res)=>{
-     res.send("User logged in successfully!");
-});
-
-app.get("/user/data",userAuth,(req,res)=>{
-   res.send("User Data Send"); 
+app.use("/",(err,req,res,next)=>{
+   if(err){
+      // logic your error
+      res.status(500).send("something went Wrong");
+   }
 })
 
-
-app.get("/admin/getAllData", (req,res)=>{
-   //  Logic of checking if the request is authorized
-      res.send("All Data Send");
- 
-});
-
-app.get("/admin/deleteUser", (req,res)=>{
-   //   Logic of checking if the request is authorized.
-      res.send("Deleted a user");
-});
-
+app.get("/getUserData",(req,res)=>{
+   try{
+      // Logic and db call and get user data
+      throw new Error("xyxhhas");
+      res.send("send User Data");
+   }
+   catch(err){
+      res.status(500).send("Some Error contact support team");
+   }
+})
+app.use("/", (err,req,res,next)=>{
+   if(err){
+      // logic your error
+      res.status(500).send("Something Went Wrong");
+   }
+})
 app.listen(7777,()=>{
     console.log("This is the listen of the 7777..")
 })
