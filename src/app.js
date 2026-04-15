9e061718e91f17cc001ae8455f904cafc66a0974
Bhautik Vekariya <vbhautik41@gmail.com>
2026-04-15 16:08:08 +0530

    Api level sanitization

--- src/app.js
+++ src/app.js
@@ -5,90 +5,108 @@ const User = require("./models/user")
 
 app.use(express.json());
 
-app.post("/signup", async (req,res)=>{
+app.post("/signup", async (req, res) => {
     console.log(req.body);
 
     // Creating a new instance of the User model
-     const user = new User(req.body);
-  try{
-      await user.save();
-       res.send("User Added Successfully!");
-  }catch(err){
-    res.status(400).send("Error Saving the User:"+ err.message)
-  }   
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.send("User Added Successfully!");
+    } catch (err) {
+        res.status(400).send("Error Saving the User:" + err.message)
+    }
 });
 
 // Get user by email 
-app.get("/user", async (req,res)=>{
+app.get("/user", async (req, res) => {
     const userEmail = req.body.emailId;
 
-    try{
+    try {
         // console.log(userEmail);
-        const users = await User.findOne({emailId: userEmail}).exec();
+        const users = await User.findOne({ emailId: userEmail }).exec();
 
-        if(!users){
+        if (!users) {
             res.status(404).send("User not found");
-        }else{
+        } else {
 
             res.send(users);
-        }    
-    //   const users = await User.find({emailId: userEmail})
-    //   if(users.length === 0){
-    //     res.status(404).send("User not found")
-    //   }else{
-    //     res.send(users);
-    //   }
-    }catch(err){
+        }
+        //   const users = await User.find({emailId: userEmail})
+        //   if(users.length === 0){
+        //     res.status(404).send("User not found")
+        //   }else{
+        //     res.send(users);
+        //   }
+    } catch (err) {
         res.status(404).send("Somethig went wrong");
     }
 })
 
 
 // Feed API - GET/feed - get all the users from the database
-app.get("/feed", async (req,res)=>{
-    try{
-      const users = await User.find({});
-      res.send(users);
-    }catch(err){
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
         res.status(404).send("Somethig Went Wrong")
     }
 })
 
 // Delete a user from the database
-app.delete("/user", async (req,res)=>{
+app.delete("/user", async (req, res) => {
     const userId = req.body.userId;
-    try{
-      const user = await User.findByIdAndDelete({_id:userId});
-      res.send("user deleted Successfully");
-    }catch(err){
+    try {
+        const user = await User.findByIdAndDelete({ _id: userId });
+        res.send("user deleted Successfully");
+    } catch (err) {
         res.status(404).send("Something went wrong");
     }
 })
 
 // Update data of a user
 
-app.patch("/user", async (req,res)=>{
-    const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params?.userId;
     const data = req.body;
     console.log(data);
-    try{
-       const user = await User.findByIdAndUpdate({_id:userId}, data, {returnDocument :"before", runValidators:true});
-       console.log(user)
+    try {
+        const ALLOWED_UPDATES = [
+            "userId", 
+            "photoUrl",
+             "about", 
+             "gender", 
+             "age", 
+             "skills",
+        ];
+
+        const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+
+        if (!isUpdateAllowed) {
+            throw new Error("Update not allowed");
+        }
+        if (data.skills && data?.skills.length > 10) {
+            throw new Error("Skills can't be more than 10");
+        }
+
+        const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: "before", runValidators: true });
+        console.log(user)
         res.send("User updated successfully")
-    }catch(err){
+    } catch (err) {
         res.status(404).send("Something went wrong");
     }
 })
 
 connetDB()
-.then(()=>{
-    console.log("Database connection established..")
-    app.listen(7777,()=>{
-    console.log("This is the listen of the 7777..")
-})
-})
-.catch((err)=>{
-    console.log("Database cannot be connted." + err)
-})
+    .then(() => {
+        console.log("Database connection established..")
+        app.listen(7777, () => {
+            console.log("This is the listen of the 7777..")
+        })
+    })
+    .catch((err) => {
+        console.log("Database cannot be connted." + err)
+    })
 
 
