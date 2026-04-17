d559fb9e7ee8b507930fd0dfd0cc5a28c9f5d598
Bhautik Vekariya <vbhautik41@gmail.com>
2026-04-17 17:07:19 +0530

    Exploring Validator Library

--- src/models/user.js
+++ src/models/user.js
@@ -1,56 +1,72 @@
 const mongoose = require("mongoose")
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
-     firstName:{
-        type: String,
-        required:true,
-        minLength:4,
-        maxLength:50,
-     },
-     lastName:{
-        type: String,
-        minLength:4,
-        maxLength:50,
-     },
-     emailId:{
-        type: String,
-        lowercase:true,
-        required:true,
-        unique:true,
-        trim:true,
-        match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"]
-     },
-     password:{
-        type: String,
-        required:true,
-        minlength:6
-     },
-     age:{
-        type: Number,
-        min: 18
-     },
-     gender:{
-        type: String,
-        validate(value){
-            if(!["male","female","other"].includes(value)){
-                throw new Error("Gender data is not valid")
-            }
-        }
-     },
-     photoUrl:{
-        type: String,
-        default:"https://www.clipartmax.com/png/full/144-1442578_flat-person-icon-download-dummy-man.png",
-     },
-     about :{
-        maxLength:150,
-        type: String,
-        default:"This is a default about of user!",
-     },
-     skills :{
-        type:[String]
-     }
+   firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+   },
+   lastName: {
+      type: String,
+      minLength: 4,
+      maxLength: 50,
+   },
+   emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"],
+      validate(value) {
+         if (!validator.isEmail(value)) {
+            throw new Error("Invalid email address:" + value);
+         }
+      }
+   },
+   password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      validate(value) {
+         if (!validator.isStrongPassword(value)) {
+            throw new Error("Enter a strong Password:" + value);
+         }
+      }
+   },
+   age: {
+      type: Number,
+      min: 18
+   },
+   gender: {
+      type: String,
+      validate(value) {
+         if (!["male", "female", "other"].includes(value)) {
+            throw new Error("Gender data is not valid")
+         }
+      }
+   },
+   photoUrl: {
+      type: String,
+      default: "https://www.clipartmax.com/png/full/144-1442578_flat-person-icon-download-dummy-man.png",
+      validate(value) {
+         if (!validator.isURL(value)) {
+            throw new Error("Invalid photo URL:" + value)
+         }
+      }
+   },
+   about: {
+      maxLength: 150,
+      type: String,
+      default: "This is a default about of user!",
+   },
+   skills: {
+      type: [String]
+   }
 
-},{
+}, {
    timestamps: true,
 });
 
